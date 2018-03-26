e971778e02b28d478f9afb99100dbced1041b0ca
Jeff King <peff@peff.net>
2018-03-26 03:29:48 -0400

    branch: make "-l" a synonym for "--list"
    The other "mode" options of git-branch have short-option
    aliases that are easy to type (e.g., "-d" and "-m"). Let's
    give "--list" the same treatment.
    
    This also makes it consistent with the similar "git tag -l"
    option.
    
    We didn't do this originally because "--create-reflog" was
    squatting on the "-l" option. Now that sufficient time has
    passed with that alias removed, we can finally repurpose it.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Reviewed-by: Eric Sunshine <sunshine@sunshineco.com>
    Reviewed-by: Jacob Keller <jacob.keller@gmail.com>
    [jc: adjusted the new tests added earlier]
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t3200-branch.sh
+++ t/t3200-branch.sh
@@ -45,12 +45,6 @@ test_expect_success 'git branch HEAD should fail' '
 	test_must_fail git branch HEAD
 '
 
-test_expect_success 'git branch -l no longer is --create-reflog' '
-	test_when_finished "git branch -D new-branch-with-reflog || :" &&
-	test_must_fail git branch -l new-branch-with-reflog &&
-	test_must_fail git rev-parse --verify refs/heads/new-branch-with-reflog
-'
-
 cat >expect <<EOF
 $_z40 $HEAD $GIT_COMMITTER_NAME <$GIT_COMMITTER_EMAIL> 1117150200 +0000	branch: Created from master
 EOF
@@ -294,7 +288,7 @@ test_expect_success 'git branch --list -v with --abbrev' '
 
 '
 
-test_expect_failure 'git branch -l eventually is --list' '
+test_expect_success 'git branch -l is --list' '
 	git branch --list >expect &&
 	git branch -l >actual &&
 	test_cmp expect actual
